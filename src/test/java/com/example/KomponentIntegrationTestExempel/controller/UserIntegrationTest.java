a25a40c24eff9e8fc4bf1d6d7b16c8c3c5ece9b8
nz-bill <bill.palmestedt@zocom.se>
2025-04-30 13:43:49 +0200

    added h2 database to test properties and added workflow file

--- src/test/java/com/example/KomponentIntegrationTestExempel/controller/UserIntegrationTest.java
+++ src/test/java/com/example/KomponentIntegrationTestExempel/controller/UserIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.net.http.HttpResponse;
